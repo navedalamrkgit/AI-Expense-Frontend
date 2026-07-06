af53c46e59ab52efdd23e8932fb498cfab19c5c1
Naved Alam <iamnavedkhn@email.com>
2026-07-06 11:30:34 -0700

    api added

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://ai-expense-backend-zuzp.onrender.com",
+    baseURL: "https://ai-expense-backend-zuzp.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
